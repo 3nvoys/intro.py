3d1128277cfbdad993ece3e2f4194c844c28551e
Firza Ananda <94677921+3nvoys@users.noreply.github.com>
2021-11-19 16:46:00 +0700

    Add files via upload

---
+++ tugas awal.py
@@ -0,0 +1,23 @@
+Nama = "Firza Ananda Bramuda" # string karena diapit tanda petik(bisa petik 1 atau petik 2)
+print(Nama)
+
+NIM = 212410102071 # integer karena hanya berupa angka(bilangan bulat) tanpa titik atau bukan desimal(kalau ada titiknya nanti jadi float) juga karena tidak diapit tanda petik 1 atau 2
+print(NIM)
+
+Umur = 18 # integer karena hanya berupa angka(bilangan bulat) tanpa titik atau bukan desimal(juga karena tidak diapit tanda petik 1 atau 2)
+print(Umur)
+
+TinggiBadan = 1.79 # float karena angka desimal(ada titknya)
+print(TinggiBadan)
+
+BeratBadan = 56 # integer karena hanya berupa angka(bilangan bulat) tanpa titik atau bukan desimal(juga karena tidak diapit tanda petik 1 atau 2)
+print(BeratBadan)
+
+StatusKejombloan = True # string karena diapit tanda petik(bisa petik 1 atau petik 2)
+print(StatusKejombloan)
+
+
+
+
+
+
